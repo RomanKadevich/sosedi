c2f4399b1e3e23ccba09f65fa446a02eff09925e
Artem <artembonda487@gmail.com>
2025-09-08 18:42:05 +0300

    fix: fix adaptiv

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ const profileButton = document.querySelector('.header__button-profile');
 const header = document.querySelector('.header');
 const heroInner = document.querySelector('.hero__inner');
 const body = document.querySelector('body');
+const hero = document.querySelector('.hero');
 
 
 burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
     body.classList.toggle('active');
     burgerLine.classList.toggle('active');
     header.classList.toggle('active');
@@ -16,6 +18,21 @@ burger.addEventListener('click', () => {
     list.classList.toggle('active');
     profileButton.classList.toggle('active');
     heroInner.classList.toggle('active');
+    hero.classList.toggle('active');
+});
+
+list.addEventListener('click', (event) => {
+    const li = event.target.closest('li');
+    if (li) {
+        body.classList.remove('active');
+        burgerLine.classList.remove('active');
+        header.classList.remove('active');
+        menu.classList.remove('active');
+        list.classList.remove('active');
+        profileButton.classList.remove('active');
+        heroInner.classList.remove('active');
+        hero.classList.remove('active');
+    }
 });
 
 
